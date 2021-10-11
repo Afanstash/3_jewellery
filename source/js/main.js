86b9fae454bf27e7e59b9069f679fb2ea54845da
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-10-12 01:29:35 +0700

    Главная страница(2 часть)

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,43 @@
 var accordions = document.querySelectorAll('.faq__item');
 
 (function () {
-  var b = accordions;
-  return b;
+  var closeAccordions = function () {
+    accordions.forEach(function (i) {
+      i.classList.remove('faq__item--active');
+    });
+  };
+
+  accordions.forEach(function (item) {
+    // var accordionTitle = document.querySelectorAl('h3');
+    item.classList.add('faq__item--js');
+    // accordionTitle.addEventListener('click', function () {
+    item.addEventListener('click', function () {
+      if (item.classList.contains('faq__item--active')) {
+        item.classList.remove('faq__item--active');
+        return;
+      }
+      closeAccordions();
+      item.classList.add('faq__item--active');
+    });
+  });
+})();
+
+(function () {
+  var closeAccordions = function () {
+    accordions.forEach(function (i) {
+      i.classList.remove('faq__item--active');
+    });
+  };
+
+  window.addEventListener('keydown', function (evt) {
+    var item = evt.target;
+    if (evt.keyCode === 13) {
+      if (item.classList.contains('faq__item--active')) {
+        item.classList.remove('faq__item--active');
+        return;
+      }
+      closeAccordions();
+      item.classList.add('faq__item--active');
+    }
+  });
 })();
--- source/js/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var accordions = document.querySelectorAll('.faq__item');
-
-(function () {
-  var b = accordions;
-  return b;
-})();
