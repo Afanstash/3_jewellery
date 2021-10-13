7da6c118f256a5ebe2501788db282e92fe6857c9
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-10-13 22:13:05 +0700

    Каталог (3 часть)

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 var accordions = document.querySelectorAll('.faq__item');
+var accordionsFilter = document.querySelectorAll('.filter__form-item');
 
 (function () {
   var closeAccordions = function () {
@@ -39,6 +40,46 @@ var accordions = document.querySelectorAll('.faq__item');
       }
       closeAccordions();
       item.classList.add('faq__item--active');
+    }
+  });
+})();
+
+(function () {
+  var closeAccordions = function () {
+    accordionsFilter.forEach(function (i) {
+      i.classList.remove('filter__form-item--active');
+    });
+  };
+
+  accordionsFilter.forEach(function (item) {
+    item.classList.add('filter__form-item--js');
+    item.addEventListener('click', function () {
+      if (item.classList.contains('filter__form-item--active')) {
+        item.classList.remove('filter__form-item--active');
+        return;
+      }
+      closeAccordions();
+      item.classList.add('filter__form-item--active');
+    });
+  });
+})();
+
+(function () {
+  var closeAccordions = function () {
+    accordionsFilter.forEach(function (i) {
+      i.classList.remove('filter__form-item--active');
+    });
+  };
+
+  window.addEventListener('keydown', function (evt) {
+    var item = evt.target;
+    if (evt.keyCode === 13) {
+      if (item.classList.contains('filter__form-item--active')) {
+        item.classList.remove('filter__form-item--active');
+        return;
+      }
+      closeAccordions();
+      item.classList.add('filter__form-item--active');
     }
   });
 })();
