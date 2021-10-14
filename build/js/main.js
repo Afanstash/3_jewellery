a0eebf813bbbcf91fb97f26313832e54b839fafc
Anastasiia Ushakova <anastasiia.b.ushakova@gmail.com>
2021-10-14 15:00:45 +0700

    Логин

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,96 @@
 'use strict';
+var body = document.querySelector('body');
 var accordions = document.querySelectorAll('.faq__item');
 var accordionsFilter = document.querySelectorAll('.filter__form-item');
+var linkLogin = document.querySelector('.main-nav__link-login');
+var modalForm = document.querySelector('.modal-form');
+var inputEmailLogin = modalForm.querySelector('#email-login');
+var inputPasswordLogin = modalForm.querySelector('#password');
+var isStorageSupport = true;
+var buttonClose = modalForm.querySelector('.modal-form__button-close');
+
+(function () {
+  linkLogin.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalForm.classList.remove('modal-form--hidden');
+    modalForm.classList.add('modal-form--show');
+    body.classList.add('lock');
+    inputEmailLogin.focus();
+  });
+})();
+
+(function () {
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      if (modalForm.classList.contains('modal-form--show')) {
+        evt.preventDefault();
+        modalForm.classList.add('modal-form--hidden');
+        modalForm.classList.remove('modal-form--show');
+        body.classList.remove('lock');
+      }
+    }
+  });
+})();
+
+(function () {
+  modalForm.addEventListener('click', function (evt) {
+    if (evt.target === modalForm) {
+      modalForm.classList.add('modal-form--hidden');
+      modalForm.classList.remove('modal-form--show');
+      body.classList.remove('lock');
+    }
+  });
+})();
+
+(function () {
+  buttonClose.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    modalForm.classList.remove('modal-form--show');
+    modalForm.classList.add('modal-form--hidden');
+    body.classList.remove('lock');
+  });
+})();
+
+(function () {
+  var resetForm = function () {
+    inputEmailLogin.value = '';
+    inputPasswordLogin.value = '';
+  };
+
+  modalForm.addEventListener('submit', function (evt) {
+    if (!inputEmailLogin.value || !inputPasswordLogin.value) {
+      evt.preventDefault();
+    } else {
+      evt.preventDefault();
+      modalForm.classList.remove('modal-form--show');
+      modalForm.classList.add('modal-form--hidden');
+      body.classList.remove('lock');
+      if (isStorageSupport) {
+        localStorage.setItem('inputEmailLogin', inputEmailLogin.value);
+      }
+    }
+
+    var formData = new FormData(evt.target);
+
+    fetchData('https://echo.htmlacademy.ru/',
+        {
+          method: 'POST',
+          body: formData,
+        }, function () {
+          resetForm();
+        });
+  });
+
+  var fetchData = function (url, options, onSuccess) {
+    fetch(url, options)
+        .then(function (response) {
+          response.json();
+        })
+        .then(function (response) {
+          onSuccess(response);
+        });
+  };
+})();
 
 (function () {
   var closeAccordions = function () {
@@ -10,9 +100,7 @@ var accordionsFilter = document.querySelectorAll('.filter__form-item');
   };
 
   accordions.forEach(function (item) {
-    // var accordionTitle = document.querySelectorAl('h3');
     item.classList.add('faq__item--js');
-    // accordionTitle.addEventListener('click', function () {
     item.addEventListener('click', function () {
       if (item.classList.contains('faq__item--active')) {
         item.classList.remove('faq__item--active');
@@ -120,7 +208,6 @@ var accordionsFilter = document.querySelectorAll('.filter__form-item');
     },
 
     autoHeight: 'true',
-
 
     // Responsive breakpoints
     breakpoints: {
